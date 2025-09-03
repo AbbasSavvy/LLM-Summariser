a42b2353ffd7015d4f2c94261845ff1fccb00680
AbbasSavvy <abbasteen@gmail.com>
2025-09-03 10:49:06 +0100

    Trying to bypass the bot block on websites

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_article(url: str, max_chars: int = 2000) -> str:
+def get_article(url: str, max_chars: int = 20000) -> str:
     """
     Scrape all paragraphs from a webpage and return as a single string.
     Limits the text to max_chars for LLM input.
     """
-    response = requests.get(url)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/139.0.0.0 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers)
 
     # Ensure request was successful
     response.raise_for_status()
